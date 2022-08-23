57874044f0366635051fe9e03dc98fc4465787b8
ale3xdyo <ale3xdyo@gmail.com>
2022-08-24 02:06:17 +0300

    Last details

--- app.js
+++ app.js
@@ -12,40 +12,75 @@ let playerSelection = "";
 let playerScore = 0;
 let computerScore = 0;
 
-const playerScoreboard = document.querySelector('.playerScore');
-const computerScoreboard = document.querySelector('.computerScore');
+const playerScoreboard = document.querySelector('#playerScore');
+const computerScoreboard = document.querySelector('#computerScore');
+
+const announcements = document.querySelector('.announcements');
+const winR = document.createElement('p');
+winR.innerHTML = 'You <span class="win">won</span> this round!';
+const loseR = document.createElement('p');
+loseR.innerHTML = 'You <span class="lose">lost</span> this round!';
+const tieR = document.createElement('p');
+tieR.innerHTML = 'it was a <span class="tie">tie</span>!';
+
+const winM = document.createElement('p');
+winM.innerHTML = 'You <span class="win">won</span> the match! Good job!';
+const replayBtn = document.createElement('button');
+replayBtn.textContent = "Play Again";
+replayBtn.classList.add('replayBtn');
+const loseM = document.createElement('p');
+loseM.innerHTML = 'You <span class="lose">lost</span> the match! Better Luck next Time!';
 
 function scoreKeeper(outcome) {
     if (playerScore < 5 && computerScore < 5)
         if (outcome === 'computer') {
             computerScore += 1;
             computerScoreboard.textContent = computerScore;
+            announcements.replaceChildren(loseR);
         }
         else if (outcome === 'player') {
             playerScore += 1;
             playerScoreboard.textContent = playerScore;
+            announcements.replaceChildren(winR);
         }
-        else console.log("Tie!");
+        else announcements.replaceChildren(tieR);
 
-    if (playerScore === 5) console.log("Player Wins");
-    else if (computerScore === 5) console.log("Computer Wins");
+    if (playerScore > computerScore) {
+        playerScoreboard.className = "win";
+        computerScoreboard.className = "lose";
+    } else if (playerScore < computerScore) {
+        playerScoreboard.className = "lose";
+        computerScoreboard.className = "win";
+    } else {
+        playerScoreboard.className = "tie";
+        computerScoreboard.className = "tie";
+    }
+
+    if (playerScore === 5) {
+        announcements.replaceChildren(winM);
+        announcements.appendChild(replayBtn);
+    }
+    else if (computerScore === 5) {
+        announcements.replaceChildren(loseM);
+        announcements.appendChild(replayBtn);
+    }
 }
 
-const btn1 = document.querySelector(".rock");
+const btn1 = document.querySelector("button.rock");
 btn1.addEventListener('click', () => {
     playerSelection = "ROCK";
     computerSelection = getComputerChoice();
     let outcome = playRound(playerSelection, computerSelection);
     scoreKeeper(outcome);
 });
-const btn2 = document.querySelector(".paper");
+const btn2 = document.querySelector("button.paper");
 btn2.addEventListener('click', () => {
     playerSelection = "PAPER";
     computerSelection = getComputerChoice();
     let outcome = playRound(playerSelection, computerSelection);
     scoreKeeper(outcome);
 });
-const btn3 = document.querySelector(".scissors");
+const btn3 = document.querySelector("button.scissors");
 btn3.addEventListener('click', () => {
     playerSelection = "SCISSORS";
     computerSelection = getComputerChoice();
@@ -53,6 +88,15 @@ btn3.addEventListener('click', () => {
     scoreKeeper(outcome);
 });
 
+replayBtn.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    computerScoreboard.textContent = computerScore;
+    playerScoreboard.textContent = playerScore;
+    computerScoreboard.className = "";
+    playerScoreboard.className = "";
+    announcements.replaceChildren();
+});
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
